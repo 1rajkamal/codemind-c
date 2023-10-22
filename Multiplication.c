52a94c2f54e4c4c34515d1f34f53c3bf79f51fac
Raj kamal <147469541+1rajkamal@users.noreply.github.com>
2023-10-22 12:31:31 +0530

    #include <stdio.h> int main() {     double num1, num2, result;     scanf("%lf", &num1);     scanf("%lf", &num2);     result = num1 * num2;     printf("%.lf",result);     return 0; }

---
+++ Multiplication.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    double num1, num2, result;
+    scanf("%lf", &num1);
+    scanf("%lf", &num2);
+    result = num1 * num2;
+    printf("%.lf",result);
+    return 0;
+}
