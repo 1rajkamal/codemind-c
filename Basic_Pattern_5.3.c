df24cddf4b426df3a1cde75ad5f24f783203a558
Raj kamal <147469541+1rajkamal@users.noreply.github.com>
2023-12-13 17:32:50 +0530

    #include <stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for (i = n; i >= 1; i--) {         for (j = 1; j <= i; j++) {             printf("%d",i);         }         for (k = 1; k <= n - i; k++) {             printf("*");         }         printf(" ");     }
        return 0;
    }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n,i,j,k;
+    scanf("%d",&n);
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("%d",i);
+        }
+        for (k = 1; k <= n - i; k++) {
+            printf("*");
+        }
+        printf("
+");
+    }
+    
+    return 0;
+}
