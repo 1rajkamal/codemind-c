00255fbd3d9403b761d2f2a92cdf0eecbb1c61b6
Raj kamal <147469541+1rajkamal@users.noreply.github.com>
2024-01-02 14:44:03 +0530

    #include <stdio.h> int countOccurrences(int arr[], int size, int target) {     int count = 0;     for (int i = 0; i < size; i++) {         if (arr[i] == target) {             count++;         }     }     return count; } int main() {     int size;     scanf("%d", &size);     int arr[size];     for (int i = 0; i < size; i++) {         scanf("%d", &arr[i]);     }     int target;     scanf("%d", &target);     int occurrences = countOccurrences(arr, size, target);     printf("%d ",occurrences); }

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int countOccurrences(int arr[], int size, int target) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int target;
+    scanf("%d", &target);
+    int occurrences = countOccurrences(arr, size, target);
+    printf("%d
+",occurrences);
+}
