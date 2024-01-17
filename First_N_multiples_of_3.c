d9f254f89e1f25e92dc1f9ea310132d8e95b2f78
Raj kamal <147469541+1rajkamal@users.noreply.github.com>
2024-01-17 10:10:12 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         printf("%d ",3*i);     } }

--- First_N_multiples_of_3.c
+++ First_N_multiples_of_3.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-    int n, i;
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++) 
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        printf("%d ", 3 * i);
+        printf("%d ",3*i);
     }
 }
