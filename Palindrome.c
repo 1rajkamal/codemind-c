a66e42805f588fb940a7f2e1493b166ae4e68510
Raj kamal <147469541+1rajkamal@users.noreply.github.com>
2024-01-18 22:48:05 +0530

    #include<stdio.h> int main() {     int n,r;     scanf("%d",&n);     int s = 0;     int m =n;     while(n!=0)     {         r = n%10;         s = (s*10)+r;         n = n/10;     }     if(m==s)     printf("Palindrome");     else     printf("Not Palindrome");     return 0; }

--- Palindrome.c
+++ Palindrome.c
@@ -1,32 +1,19 @@
-#include <stdio.h>
-
-int isPalindrome(int num) {
-    int reversedNum = 0, remainder, originalNum;
-
-    originalNum = num;
-
-    while (num > 0) {
-        remainder = num % 10;
-        reversedNum = reversedNum * 10 + remainder;
-        num /= 10;
+#include<stdio.h>
+int main()
+{
+    int n,r;
+    scanf("%d",&n);
+    int s = 0;
+    int m =n;
+    while(n!=0)
+    {
+        r = n%10;
+        s = (s*10)+r;
+        n = n/10;
     }
-
-    if (originalNum == reversedNum)
-        return 1; // It's a palindrome
-    else
-        return 0; // It's not a palindrome
-}
-
-int main() {
-    int number;
-    scanf("%d", &number);
-
-    if (isPalindrome(number))
-        printf("True
-", number);
-    else
-        printf("False
-", number);
-
+    if(m==s)
+    printf("Palindrome");
+    else 
+    printf("Not Palindrome");
     return 0;
 }
