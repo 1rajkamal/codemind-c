1176ccc2a0566aad73f5c570c824aba80b488470
Raj kamal <147469541+1rajkamal@users.noreply.github.com>
2024-01-19 22:41:43 +0530

    #include<stdio.h> int main() {     int l,n,i;     scanf("%d %d",&l,&n);     for(i=1;i<=n;i++)     {         int w,h;         scanf("%d %d",&w,&h);         if(w<l||h<l)        printf("UPLOAD ANOTHER ");         else if(w==h)         printf("ACCEPTED ");         else         printf("CROP IT ");     } }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,i;
+    scanf("%d %d",&l,&n);
+    for(i=1;i<=n;i++)
+    {
+        int w,h;
+        scanf("%d %d",&w,&h);
+        if(w<l||h<l)
+       printf("UPLOAD ANOTHER
+");
+        else if(w==h)
+        printf("ACCEPTED
+");
+        else
+        printf("CROP IT
+");
+    }
+}
